ed2b9fd0548d04c8930b3ab11a1123c7d8243fee
Shipu <shipu.johirul@gmail.com>
2021-09-13 12:51:43 +0600

    final check and update

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
